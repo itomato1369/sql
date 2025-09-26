a2005a7aff9327ba0a98ba63458937382748db7b
doowoo <itomato1369@gmail.com>
2025-09-26 18:01:00 +0900

    0926

--- scott.sql
+++ scott.sql
@@ -1,136 +1,359 @@
---SELECT  empno  as  "사원번호"   --  별명 별칭 (alias)
---                 ,ename  as   "사원명"
---                 ,'Good Morning !!'  | |  ename  as  "welcome 메세지"-- 연결하고 싶을 때는 | |    
---                 ,ename  | |  ' ''의 급여 '  | |  sal  as "급여"  -- 영문일 경우  KIM's salary
---FROM  emp; 
--- SELECT 는 FROM과 같이 씀
+SELECT s.name
+,s.deptno1
+,d.dname
+FROM student s
+JOIN department d
+ON s.deptno1 = d.deptno;
 
---SELECT  empno
---,hiredate  as  "hd"
---FROM  emp;
+SELECT s.name
+,s.deptno1
+,d.dname
+FROM student s, department d
+WHERE s.deptno1 = d.deptno;
 
---  DISTINCT 중복된 값을 제거  ORDER BY 정렬 DESC 역순
---SELECT  DISTINCT    job,    deptno
---FROM    emp
---ORDER BY    job DESC;
+-- DDL 0926
+SELECT *
+FROM tab;
 
+SELECT *
+FROM new_table1;
+ALTER TABLE new_table1 ADD info GENERATED ALWAYS AS (no || '-' || name);
 
---SELECT name|| ' ''s ID : ' || id|| 'WEIGHT is' || weight || 'kg' 
---AS "ID AND WEIGHT"
---FROM student;
---
---SELECT ename || ' ( ' ||  job || '), ' || ename || '''' || job || ''''
---AS "NAME AND JOB"
---FROM emp;
---
---SELECT ename || ' ''s  salary is $' || sal
---AS "Name And Sal"
---FROM emp;
---
----- 조건절 conditional expression
---SELECT empno
---, ename
---, job
---, mgr
---, hiredate
---, sal + comm AS "Salary"
-----, comm
---, deptno
---FROM emp
---WHERE empno >= 7900 AND empno < 8000 AND hiredate > '82/01/01';
----- LIKE '% ABC %' ABC 가 포함된 
----- 날짜를 조회할 때는 '' 작은 따옴표 안에 
+ROLLBACK;
 
---SELECT *
---FROM professor
---WHERE email LIKE '%naver.com%';
---WHERE pay + bonus >= 300 OR pay >= 300;
---WHERE 1=1 AND pay + NVL (bonus, 0) >= 300; 
---
---SELECT profno
---, LOWER (name) AS "low_name"
---, UPPER (id) AS "upp_id"
---, INITCAP (position) AS "pos"
---, pay
---, CONCAT(CONCAT (name, ' - '), id ) AS "name_id"
---FROM professor
---WHERE LENGTH (name) != 10;
+-- Be cautious
+DELETE FROM new_table1  
+WHERE no = 1;
 
---SELECT name
---, LENGTH (name) AS "length"
---, LENGTHB ('가나다') AS "lengthB"
---, SUBSTR (name, 1, 5) AS "substr"
---, INSTR (name, 'a') AS "instr"
---, pay
---, bonus
---, LTRIM(LPAD (id, 10, '*'), '*') AS "rpad"
---, TRIM('                                       Hello, World                             ')  AS  "str"
---, REPLACE ('Hello', 'H', 'h') AS "rep"
---FROM professor
---WHERE INSTR (UPPER (name) , 'A') > 0;
+SELECT *
+FROM user_recyclebin;  -- recyclebin
 
-SELECT name
-, tel
-, SUBSTR(tel, 1, INSTR(tel, ')') -1) AS "AREA CODE"
-FROM student
-WHERE deptno1 = 201;
+FLASHBACK TABLE new_table1 TO BEFORE DROP;  　
 
-SELECT ename
-, REPLACE(ename, SUBSTR(ename, 2, 2), ' - - ') AS "replace"
+CREATE TABLE new_emp
+( no NUMBER(4)
+  CONSTRAINT emp_no_pk PRIMARY KEY,
+name VARCHAR2(20)
+  CONSTRAINT emp_name_nn NOT NULL,
+jumin VARCHAR2(13)
+  CONSTRAINT emp_jumin_nn NOT NULL
+  CONSTRAINT emp_jumin_uk UNIQUE,
+loc_code NUMBER(1)
+  CONSTRAINT emp_area_ck CHECK (loc_code <5),
+deptno NUMBER(2) 
+  CONSTRAINT emp_dept_fk REFERENCE dept(deptno)
+);
+
+CREATE TABLE new_table1(
+    no NUMBER(3) PRIMARY KEY,   -- Membership No 1~999
+    name VARCHAR2(100) NOT NULL,  -- Name 0000000000...00000
+    birth DATE DEFAULT SYSDATE  -- TO_DATE('2025/09/26', 'RRRR/MM/DD)
+);
+ALTER TABLE new_table1
+ADD phone VARCHAR2(20);
+ALTER TABLE new_table1 RENAME COLUMN phone TO tel;
+ALTER TABLE new_table1 MODIFY tel VARCHAR2(30);
+
+ALTER TABLE new_table1 READ ONLY; -- 21c    NOT function in 11g
+    
+INSERT INTO new_table1 (no, name, birth)
+VALUES(1, 'DDD', '2011/01/01');
+
+-- Be cautious
+UPDATE new_table1  -- UPDATE
+SET phone = '010-9876-5432'
+    ,birth = TO_DATE('2002/02/02', 'RRRR/MM/DD')  -- Standard Procedure　
+--WHERE no = 2;   -- Conditional Statement
+
+DESC new_table1;  -- Check Table Structure
+
+ALTER TABLE new_table1 DROP COLUMN tel;
+
+-- Be cautious
+TRUNCATE TABLE new_table1;   -- Delete Data
+DROP TABLE new_table1;    -- Delete Table
+
+------------------------------------------------
+--DML
+SELECT *
+FROM dept2
+ORDER BY dcode;
+DESC dept2;
+
+INSERT INTO dept2 (dcode, dname, pdept, area) -- Insert new data 
+VALUES('9000', 'temp_1', '1006', 'temp area');
+
+CREATE TABLE professor3  -- CTAS = duplicate
+AS
+SELECT *
+FROM professor
+WHERE 1 = 2;         
+
+INSERT INTO professor3 -- ITAS 
+SELECT *
+FROM professor;
+
+SELECT *
+FROM professor3;
+
+CREATE TABLE prof_1 (
+ profno NUMBER,
+ name VARCHAR2(25));
+ 
+CREATE TABLE prof_2 (
+ profno NUMBER,
+ name VARCHAR2(25));
+ 
+INSERT ALL          
+ WHEN profno BETWEEN 1000 AND 1999 THEN
+  INTO prof_1 VALUES (profno, name)
+ WHEN profno BETWEEN 2000 AND 2999 THEN
+  INTO prof_2 VALUES (profno, name)
+SELECT profno, name
+FROM professor;
+
+SELECT *
+FROM prof_1;
+SELECT *
+FROM prof_2;
+
+INSERT ALL          
+  INTO prof_1 VALUES (profno, name) 
+  INTO prof_2 VALUES (profno, name)
+SELECT profno
+,name
+FROM professor;
+
+SELECT *
+FROM professor;
+
+-- Be cautious
+UPDATE professor
+SET bonus = DECODE(bonus, null, 100, bonus)
+,pay = pay + (pay * 0.1)
+WHERE 1 = 1;
+
+-- Be cautious
+DELETE FROM professor
+WHERE hpage IS not null;
+
+-- DELETE dept　
+SELECT *
+FROM dept;
+
+SELECT *
+FROM emp;
+ 
+SELECT * 
+FROM emp
+WHERE deptno = 30;
+
+UPDATE emp
+SET deptno = 50
+WHERE deptno = 20; 
+
+SELECT *
+FROM emp e, dept d
+WHERE e.deptno = d.deptno;
+
+-- EXISTS   true or false
+UPDATE emp e
+SET sal = sal + 100
+WHERE EXISTS (SELECT 1
+FROM dept d
+WHERE e.deptno = d.deptno
+AND d.loc = 'DALLAS');
+
+-- d.loc is not 'DALLAS'
+WHERE NOT EXISTS (SELECT 1
+FROM dept d
+WHERE e.deptno = d.deptno
+AND d.loc = 'DALLAS');
+
+CREATE TABLE new_emp1 (
+no NUMBER(4) CONSTRAINT emp1_no_pk PRIMARY KEY,
+name VARCHAR2(20) CONSTRAINT emp1_name_nn NOT NULL,
+jumin VARCHAR2(13) CONSTRAINT emp1_jumin_nn NOT NULL
+CONSTRAINT emp1_jumin_uk UNIQUE,
+loc_code NUMBER(1) CONSTRAINT emp1_area_ck CHECK (loc_code < 5),
+deptno VARCHAR2(6) CONSTRAINT emp1_deptno_fk REFERENCES dept2(dcode)
+);
+
+-- Bulletin board 게시판
+-- Oracle Server -- Web Server(Node.js) -- Client(fetch)
+SELECT *
 FROM emp;
 
-SELECT name
-, jumin
-, RPAD(SUBSTR(jumin, 1, 6), 20, ' - /') AS "replace"
-FROM student
-WHERE deptno1 = 101;
 
-SELECT name
-, jumin
-, REPLACE(jumin, SUBSTR(jumin, 7, 7), ' -/-/-/- ') AS "replace"
-FROM student
-WHERE deptno1 = 101;
 
-SELECT name
-, tel
-, REPLACE(tel, SUBSTR(tel, 5,3), '***') AS "replace"
-FROM student
-WHERE deptno1 = 102;
 
-SELECT name
-, tel
-, REPLACE(tel, SUBSTR(tel, 9, 4), '****') AS "replace"
-FROM student
-WHERE deptno1 = 101;
 
-SELECT empno
-, ename
-, job
-, ROUND(sal / 12, 1) AS "month"
-, TRUNC(sal / 12) AS "trunc"
-, MOD(sal , 12) AS "mod"
-, CEIL(sal / 12) AS "ceil"
-,  FLOOR(sal / 12) AS "floor"
-, POWER(sal , 2) AS "power"
-FROM emp;
 
---SELECT MONTHS_BETWEEN('16/01/01','15/01/01')
---FROM dual;
--- dual 더미 데이터 RRRR/MM/DD HH:24 MI SS
 
-CHAR(2000)                   ->   '       3' '1st'
-VARCHAR2(4000)       -> ' 3 '
-NUMBER(10, 2) 12345678.90
 
-SELECT ADD_MONTHS(sysdate, 2), NEXT_DAY(sysdate+1, '목') AS "next_day"
-, LAST_DAY(ADD_MONTHS(sysdate, 1)) AS "last"
-FROM dual;
 
-SELECT sysdate, TO_CHAR (sysdate, 'RRRR/MM/DD') AS "today"
-FROM dual;
 
-SELECT TO_DATE('2025-05-05', 'RRRR-MM-DD')
-FROM dual;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
